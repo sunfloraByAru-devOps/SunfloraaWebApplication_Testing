3b8dc16fc428d26589d1cf8c0a477d2722d2bc64
Gunjan co-founder <akash2244everyone@gmail.com>
2026-06-04 14:01:12 +0530

    homepage makeover

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourdomain.com',
+  site: 'https://sunfloracrochet.in',
   integrations: [sitemap()],
 
   vite: {
